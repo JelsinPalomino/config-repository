499963e61a8e86681edea8bfada376dfd58a0a86
JelsinPalomino <jstpalomino@hotmail.com>
2025-01-14 17:40:03 -0500

    Ajustes de detailes-release and version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="foodemo30",
-    version="0.0.4",
+    version="0.0.5",
     description="Foo demo project the II",
     long_description=readme(),
     long_description_content_type="text/markdown",
