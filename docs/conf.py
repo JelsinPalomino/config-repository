74cd13c30449a3d3e2b7be2a7761f0e50e879e58
JelsinPalomino <jstpalomino@hotmail.com>
2025-01-15 08:15:57 -0500

    Configuramos flake8 para la linea11

--- docs/conf.py
+++ docs/conf.py
@@ -1,21 +1,18 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../foo'))
 
-project = 'foodemo30'
-copyright = '2025, Jelsin Palomino'
-author = 'Jelsin Palomino'
-release = 'v0.1.1'
+sys.path.insert(0, os.path.abspath("../foo"))
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
-]
+project = "foodemo30"
+copyright = "2025, Jelsin Palomino"
+author = "Jelsin Palomino"
+release = "v0.1.1"
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-language = 'es'
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+language = "es"
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
